7732ad7646909951ecc91223aa0d9b63f46b7861
IndusVR <iransing@LIN51009621.corp.capgemini.com>
2021-09-20 10:12:56 +0530

    First commit

---
+++ Branching repositary/BranchingSample/src/userlogin.java
@@ -0,0 +1,6 @@
+
+public class userlogin {
+	public static void main(Strings[] args) {
+	System.out.println("This is userlogin");
+	}
+}
---
+++ Branching repositary/branchingrepo/src/branchingrepo/branchingrepo.java
@@ -0,0 +1,7 @@
+package branchingrepo;
+
+public class branchingrepo {
+     public static void main(String[] args) {
+    	 System.out.println("This is userlogin");
+     }
+}
